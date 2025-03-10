bbf331cad0698e4b579ec6945d9eb651ac87a7bc
Juan David Segura Zabala <jdsegurazabala@gmail.com>
2025-03-10 08:53:50 -0500

    feat(*): adjustments for production

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     domains: ['s4.anilist.co'], // Agrega el dominio aquí
+    unoptimized: true, // Deshabilita la optimización de imágenes
   },
 };
 
